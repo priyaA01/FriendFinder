8af6bc9f21b2a32bc94ec41969dabcbb7d5177fe
priyaA01 <pryshanthi.it@gmail.com>
2018-02-26 22:59:20 -0600

    updated version 2

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
-app.use(express.static(__dirname + '/public/'));
+app.use(express.static("app/public/"));
 
 
 require("./app/routing/apiRoutes")(app);
